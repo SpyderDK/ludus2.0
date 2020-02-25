d66e120a738d56c3e18c0d52b202d1b1056ac835
Magnus <58548065+SpyderDK@users.noreply.github.com>
2020-02-25 11:16:32 +0100

    php test

---
+++ php test.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+
+        <p>
+            Dette er HTML. <b> <?php echo "og dette er PHP." ?> </b>
+        </p>
+
+    </body>
+</html>
