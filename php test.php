d445e234f3cacfda3f838d8e22880aef1b6d8f65
Magnus <58548065+SpyderDK@users.noreply.github.com>
2020-02-25 13:02:08 +0100

    Update php test.php

--- php test.php
+++ php test.php
@@ -4,9 +4,24 @@
     </head>
     <body>
 
-        <p>
-            Dette er HTML. <b> <?php echo "og dette er PHP." ?> </b>
-        </p>
+        <?php
+            $tal = 12;
+            $tal2 = 13;
+            $tal3 = 14;
+
+            echo $tal;
+            echo "<br>";
+
+            echo $tal2;
+            echo "<br>";
+
+            echo $tal3;
+            echo "<br>";
+
+            echo "$tal er  et tal";
+            echo '$tal er et tal';
+
+        ?>
 
     </body>
 </html>
