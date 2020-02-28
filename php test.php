68a0ac6e793442a5e3335ecd452b6c8286e647b9
Magnus <58548065+SpyderDK@users.noreply.github.com>
2020-02-28 08:44:03 +0100

    added "tasks.json"

--- php test.php
+++ php test.php
@@ -5,10 +5,13 @@
     <body>
 
         <?php
-            $tal = 12;
+
+            
+            $tal = 15;
             $tal2 = 13;
             $tal3 = 14;
 
+            /*
             echo $tal;
             echo "<br>";
 
@@ -20,7 +23,25 @@
 
             echo "$tal er  et tal";
             echo '$tal er et tal';
+            */
 
+            switch($tal)    {
+                case 12:
+                    echo "12";
+                break;
+
+                case 13:
+                    echo "13";
+                break;
+
+                case 14:
+                    echo "14";
+                break;
+
+                default:
+                    echo "Fejl";
+                break;
+            }
         ?>
 
     </body>
