00de5c0d026acf5d1649a442b6bf147bbe67611a
Magnus <58548065+SpyderDK@users.noreply.github.com>
2020-02-28 11:55:57 +0100

    øvelser

--- øvelser.php
+++ øvelser.php
@@ -28,6 +28,7 @@
     */
 
     // For-loop øvelse
+    /*
     for ($i=0; $i <= 1000; $i++) { 
         $tilbage = $i % 2;
 
@@ -36,5 +37,47 @@
             echo "<br>";
         }
     }
+    */
 
+    //funktioner
+    /*
+    function udprint() {
+        static $i = 1;
+        echo "hej nr. $i<br>";
+        $i++;
+    }
+
+    udprint();
+    udprint();
+    udprint();
+    udprint();
+
+    $a = "lord";
+
+    function praise() {
+        global $a;
+        echo "<br>";
+        echo "praise the $a";
+    }
+
+    praise();
+    */
+
+    // Arrays
+    $array[] = 24;
+    $array[] = 256;
+    $array[] = 234;
+    $array[] = 89;
+    $array[] = 12345;
+
+    //print_r($array);
+
+    echo "$array[2]<br>";
+
+    $papir["A3"] = 50;
+    $papir["A4"] = 250;
+
+    echo $papir["A4"];
+
+    print_r($papir);
 ?>
