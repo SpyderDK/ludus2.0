608e79b719ccb1a86f4fedfeeeebacc3d3c9a298
Magnus <58548065+SpyderDK@users.noreply.github.com>
2020-03-06 09:01:02 +0100

    Filer til login-side
    php dokument til startside + logo

---
+++ login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Log ind på SkoleIntra</title>
+        <meta charset="utf-8" />
+        <meta name="description" content="Log ind side til SkoleIntra" />
+    </head>
+    <body>
+
+        
+
+    </body>
+</html>
