c323936f7d7bdbb852987cab5773e673ddee1810
Magnus <58548065+SpyderDK@users.noreply.github.com>
2020-03-06 13:19:22 +0100

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Log ind på SkoleIntra</title>
+    <title>Log på SkoleIntra</title>
     <meta charset="utf-8" />
     <meta name="description" content="Log ind side til SkoleIntra" />
 </head>
@@ -11,24 +11,26 @@
 
     <div style="background-color: #232443">
         <div style="text-align: center">
-            <img src="media\logo.png" alt="Logo til SkoleIntra" />
+            <img src="./media/logo.png" alt="Logo til SkoleIntra" />
         </div>
-        <div style="text-align: center; color: #56be88">
-            <h2>Log Ind</h2>
-        </div>
-        <div style="text-align: center">
-            <form id="Brugernavn" action="#" method="post">
-                <input type="text">
-                <br>
-                <input type="password">
-                <br>
-                <div style="color: #ffffff">
-                    <input type="checkbox" value="husk kode" name="husk kode" id="husk kode">
-                    <label for="husk kode">Husk Kodeord?</label>
-                </div>
-                <br>
-                <input type="submit" value="Log Ind">
-            </form>
+        <div style="background-color: #DFE1E6; width: 400px; margin-right: auto; margin-left: auto">
+            <div style="text-align: center; color: #56be88">
+                <h2>Log Ind</h2>
+            </div>
+            <div style="text-align: center">
+                <form id="Brugernavn" action="#" method="post">
+                    <input type="text" autofocus placeholder="Brugernavn" required>
+                    <br>
+                    <input type="password" placeholder="Adgangskode" required>
+                    <br>
+                    <div>
+                        <input type="checkbox" value="husk kode" name="husk kode" id="husk kode">
+                        <label for="husk kode"><u>Husk Adgangskode?</u></label>
+                    </div>
+                    <br>
+                    <input type="submit" value="Log Ind">
+                </form>
+            </div>
         </div>
     </div>
 
