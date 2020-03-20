44d4a4d1999011ddbf666794286f48e9088c7be3
SpyderDK <58548065+SpyderDK@users.noreply.github.com>
2020-03-20 11:37:28 +0100

    Update design

--- login.php
+++ login.php
@@ -6,22 +6,23 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Log på SkoleIntra">
-    <link rel="shortcut icon" type="image/x-icon" href="./test-media/icon 48x48.ico">
-    <link rel="stylesheet" type="text/css" href="./stylesheets/test-main.css">
+    <link rel="shortcut icon" type="image/x-icon" href="./media/icon 48x48.ico">
+    <link rel="stylesheet" type="text/css" href="./stylesheets/main.css">
 </head>
 
 <body>
 
     <div id="logo">
-        <img src="./test-media/logo.png" alt="Logo til SkoleIntra">
+        <img src="./media/logo ref.png" alt="Logo til SkoleIntra" width="440" height="210">
     </div>
     <div id="loginboks">
         <h1>Log Ind</h1>
         <form id="logind" action="#" method="post">
-            <input class="inputtekst" type="text" autofocus placeholder="Brugernavn" required>
+            <input class="inputtekst" type="text" autofocus placeholder="Brugernavn">
             <br>
             <br>
-            <input class="inputtekst" type="password" placeholder="Adgangskode" required>
+            <input class="inputtekst" type="password" placeholder="Adgangskode">
+            <br>
             <br>
             <input type="checkbox" value="husk kode" name="husk kode" id="husk kode">
             <label for="husk kode"><u>Husk Adgangskode?</u></label>
