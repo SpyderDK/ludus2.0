8b380b7c7c2d304063816ceaadee81d022956bcd
SpyderDK <58548065+SpyderDK@users.noreply.github.com>
2020-03-24 10:38:44 +0100

    begyndt på forside

---
+++ forside.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Forside</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, inital-scale=1.0">
+    <meta name="description" content="Forside til skoleintra">
+    <link rel="shortcut icon" type="image/x-icon" href=".media/icon 48x48.ico">
+    <link rel="stylesheet" type="text/css" href="./stylesheets/main.css">
+</head>
+
+<body>
+    <header class="dashboard">Dashboard</header>
+    <div class="sidebar">
+        <img src=".media/hjemknap.svg" alt="logo til lectio" class="min-logo">
+    </div>
+</body>
+
+
+
+</html>
