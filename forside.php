a5040a0c258831e24694aeebe078f2a34311a637
SpyderDK <58548065+SpyderDK@users.noreply.github.com>
2020-03-24 12:16:04 +0100

    added ikon folder + update forside.php

--- forside.php
+++ forside.php
@@ -6,14 +6,33 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, inital-scale=1.0">
     <meta name="description" content="Forside til skoleintra">
-    <link rel="shortcut icon" type="image/x-icon" href=".media/icon 48x48.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="./media/favicon.ico">
     <link rel="stylesheet" type="text/css" href="./stylesheets/main.css">
 </head>
 
+
+
 <body>
-    <header class="dashboard">Dashboard</header>
-    <div class="sidebar">
-        <img src="./media/hjemknap.svg" alt="logo til lectio" class="min-logo">
+    <div class="sidebar" id="sidebar">
+        <img src="./media/ikoner/hjemknap.svg" alt="logo til lectio" class="min-logo">
+        <br>
+        <img src="./media/ikoner/hjemikon.svg" alt="hjemknap" class="hjem-knap">
+        <br>
+        <img src="./media/ikoner/placeholder.svg" alt="idk" class="placeholder-knap">
+        <br>
+        <img src="./media/ikoner/kalender.svg" alt="kalenderknap" class="kalender-knap">
+        <br>
+        <img src="./media/ikoner/beskeder.svg" alt="beskedknap" class="besked-knap">
+        <br>
+        <img src="./media/ikoner/opgaver.svg" alt="opgaveknap" class="opgave-knap">
+        <br>
+        <img src="./media/ikoner/profil.svg" alt="profilknap" class="profil-knap">
+        <br>
+        <img src="./media/ikoner/indstillinger.svg" alt="indstillinger" class="indstillinger">
+    </div>
+
+    <div class="topbar" id="topbar">
+        <header><b>Dashboard</b></header>
     </div>
 </body>
 
--- login.php
+++ login.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Log på SkoleIntra">
-    <link rel="shortcut icon" type="image/x-icon" href="./media/icon 48x48.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="./media/favicon.ico">
     <link rel="stylesheet" type="text/css" href="./stylesheets/main.css">
 </head>
 
