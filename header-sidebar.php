37b01b81b1d39e9f5d893139a95d8c506d68bb88
SpyderDK <58548065+SpyderDK@users.noreply.github.com>
2020-03-27 12:45:09 +0100

    add header-sidebar.php

---
+++ header-sidebar.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Forside</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, inital-scale=1.0">
+    <meta name="description" content="Forside til skoleintra">
+    <link rel="shortcut icon" type="image/x-icon" href="./media/favicon.ico">
+    <link rel="stylesheet" type="text/css" href="./stylesheets/main.css">
+</head>
+
+
+
+<body>
+    <div class="sidebar" id="sidebar">
+        <img src="./media/ikoner/hjemknap.svg" alt="logo til lectio" class="min-logo">
+        <br>
+        <img src="./media/ikoner/hjemikon.svg" alt="hjemknap" class="hjem-knap">
+        <br>
+        <img src="./media/ikoner/fravær.svg" alt="fraværsknap" class="fravær-knap">
+        <br>
+        <img src="./media/ikoner/kalender.svg" alt="kalenderknap" class="kalender-knap">
+        <br>
+        <img src="./media/ikoner/beskeder.svg" alt="beskedknap" class="besked-knap">
+        <br>
+        <img src="./media/ikoner/opgaver.svg" alt="opgaveknap" class="opgave-knap">
+        <br>
+        <img src="./media/ikoner/profil.svg" alt="profilknap" class="profil-knap">
+        <br>
+        <img src="./media/ikoner/indstillinger.svg" alt="indstillinger" class="indstillinger">
+    </div>
+
+    <div class="topbar" id="topbar">
+        <header>
+            <b>Dashboard</b>
+            <div class="ikoner">
+                <img src="./media/ikoner/søgikon.svg" alt="søgikon" class="søg-ikon">
+                <img src="./media/ikoner/klokke.svg" alt="notifikationer" class="notif-ikon">
+                <img src="./media/ikoner/profilikon.svg" alt="profil" class="profil-ikon">
+            </div>
+        </header>
+    </div>
+</body>
+
+
+
+</html>
