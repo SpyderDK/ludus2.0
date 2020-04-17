ebdf78d939ef405b45aa8f803c706b1ea27c0e76
Magnus <58548065+SpyderDK@users.noreply.github.com>
2020-04-17 10:08:23 +0200

    update

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -6,14 +6,14 @@ if (isset($_POST['login-submit'])) {
     $password = $_POST['pwd'];
 
     if (empty($uid) || empty($password)) {
-        header("Location: ../forside.php?error=emptyfields");
+        header("Location: ../dashboard.php?error=emptyfields");
         exit();
     } else {
         $sql = "SELECT * FROM users WHERE uidUsers=?;";
         $stmt = mysqli_stmt_init($conn);
 
         if (!mysqli_stmt_prepare($stmt, $sql)) {
-            header("Location: ../forside.php?error=sqlerror");
+            header("Location: ../dashboard.php?error=sqlerror");
             exit();
         } else {
             mysqli_stmt_bind_param($stmt, "s", $uid);
@@ -24,26 +24,26 @@ if (isset($_POST['login-submit'])) {
                 $passcheck = password_verify($password, $row['pwdUsers']);
 
                 if ($passcheck == false) {
-                    header("Location: ../forside.php?error=wrongcredentials");
+                    header("Location: ../dashboard.php?error=wrongcredentials");
                     exit();
                 } elseif ($passcheck == true) {
                     session_start();
                     $_SESSION['userId'] = $row['idUsers'];
                     $_SESSION['userUid'] = $row['uidUsers'];
 
-                    header("Location: ../forside.php?login=success");
+                    header("Location: ../dashboard.php?login=success");
                     exit();
                 } else {
-                    header("Location: ../forside.php?error=wrongcredentials");
+                    header("Location: ../dashboard.php?error=wrongcredentials");
                     exit();
                 }
             } else {
-                header("Location: ../forside.php?error=nouser");
+                header("Location: ../dashboard.php?error=nouser");
                 exit();
             }
         }
     }
 } else {
-    header("Location: ../forside.php");
+    header("Location: ../dashboard.php");
     exit();
 }
